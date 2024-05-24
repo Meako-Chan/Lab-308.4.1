2ff3816dde17f2c4ff612aab907654643686dabb
Michael Chan <michael.w.chan01@gmail.com>
2024-05-24 16:34:37 -0400

    Change keys to lowercase

--- Practical Loops.js
+++ Practical Loops.js
@@ -1,11 +1,9 @@
 
-// Part 1 & Part 2 (Pasted from past lab)
-let test = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26'
-let test1 = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232'
-
-let lines = test.split('\n');
+// Part 1 & Part 2 (Pasted from past lab)Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Q
+let test = 'ID,Name,Occupation,Age\n42,uiz Master,58\n98,Bill,Doctor’s Assistant,26'
+let test1 = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
+let lines = test1.split('\n');
 let cells = [];
-let cell1, cell2, cell3, cell4;
 for(const i of lines){
     cells.push(i.split(','));
 }
@@ -13,7 +11,12 @@ let column = cells[0].length;
 
 // console.log(cells);
 
-// Part 3
+// Part 3 Transforming Data
+//Change keys to lowercase
+for(let i = 0; i < column; i++){
+    let lowercase = cells[0][i].toLowerCase();
+    cells[0][i] = lowercase;
+}
 key_values = [];
 for(let i = 1; i < cells.length; i++){
     let person = {};
@@ -22,6 +25,11 @@ for(let i = 1; i < cells.length; i++){
     }
     key_values.push(person);
 }
-
 console.log(key_values);
+
+//Part 4 Sorting and Manipulating Data
+//Insert new object at index 1
+let person2 = {id: "48", name:"Barry"};
+
+
 
