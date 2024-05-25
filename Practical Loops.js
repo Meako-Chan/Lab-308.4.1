b963d85bc4af04f64ac47ccdde9047458943a838
Michael Chan <michael.w.chan01@gmail.com>
2024-05-25 15:39:37 -0400

    Part 4 Finished

--- Practical Loops.js
+++ Practical Loops.js
@@ -25,11 +25,33 @@ for(let i = 1; i < cells.length; i++){
     }
     key_values.push(person);
 }
-console.log(key_values);
 
 //Part 4 Sorting and Manipulating Data
 //Insert new object at index 1
-let person2 = {id: "48", name:"Barry"};
+//Remove Last Index
+key_values.pop();
+let person2 = {id: "48", name:"Barry", occupation: "Runner", age: "25" };
+//Insert person2 at index 1 of key_values
+let index = 1;
+//Split the key values, insert person2 at index1 the concat with the rest of the array.
+//Alternate way: key_values = key_values.slice(0, index).concat(person2, key_values.slice(index));
+ key_values = [...key_values.slice(0,index), person2, ...key_values.slice(index)];
+
+let person3 = {id: "7", name:"Bilbo", occupation: "None", age: "111" };
+ //Insert person3 at end of array
+ key_values.push(person3);
+
+ 
+//Calculate average age of the group
+// Alternate Solution: let average_age = key_values.reduce((sum, value) => sum + Number(value.age), 0)
+let average_age = 0;
+ for(let i=0; i < key_values.length; i++){
+    average_age += Number(key_values[i].age);
+ }
+average_age /= key_values.length;
+console.log(average_age);
+
+//Part 5:
 
 
 
